65ed777ddb6d5cd198a97d4b4acfb2511056d545
DavidSNB <43823266+DavidSNB@users.noreply.github.com>
2020-08-17 10:49:46 +0100

    Lyric Search Functionality Completed

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import lyricsgenius as genius
 import spotify_data_scraper as spotify
 import sys
+import time
 
 
 api = genius.Genius(
     "mbCzTT54Dpe6luxCvCTT_1hLX49XfvHwVZS-y1o0IZm-EebY7KKNgsrEXmNFkM_h",
     remove_section_headers=True)
 
-# imported_song_data = spotify.get_song_data()
+collected_songs = []
+not_genius = []
+matching_songs = []
+
+
+def quit():
+    print("Closing Program...")
+    time.sleep(3)
+    sys.exit()
 
 
 def initial_import():
@@ -16,29 +25,48 @@ def initial_import():
             "Have you imported your up-to-date spotify data? [y/N]\n").lower()
 
         if imported == "y":
-            print("Yay")
+            print("Continuing...")
+            imported_properly = True
         elif imported == "n" or not imported:
-            test = spotify.get_spotify_file()
-            print(test)
+            imported_properly = spotify.get_spotify_file()
         else:
-            print("Process Aborted!")
-            print("=======================================================")
-            print("\n")
+            print("Unknown Input\n")
             initial_import()
     else:
         input("You need to import a spotify JSON file! [PRESS ENTER]")
-        spotify.get_spotify_file(True)
+        imported_properly = spotify.get_spotify_file(True)
 
-    find_lyrics(spotify.get_song_data())
+    if not imported_properly:
+        resolved_input = False
+
+        while not resolved_input:
+            proceed = input(
+                "You did not import a file, do you still want to continue? [Y/n]\n").lower()
+            if proceed == "y" or not proceed:
+                continue
+            elif proceed == "m":
+                quit()
+            else:
+                print("Unknown Input\n")
+
+    select_function()
 
 
 def select_function():
     function = input(
-        "What would you like to do with your data?\n[A] Find the song based on a lyric")
+        "What would you like to do with your data?\n[A] Find the song based on a lyric\n[q] Nothing\n").lower()
+
+    if function == "a" or not function:
+        lyric_search()
+    elif function == "q":
+        quit()
+    else:
+        print("Unknown Input\n")
+        select_function()
 
 
 """
-def find_lyrics(songs, collected=[], count=0):
+def find_lyrics(songs, collected_songs=[], count=0):
     not_recognised = []
 
     total = len(songs)
@@ -79,45 +107,49 @@ def find_lyrics(songs, collected=[], count=0):
 """
 
 
-def find_lyrics(songs, collected=[], count=0):
-    not_recognised = []
+def collect_lyrics(songs=spotify.get_song_data()):
+    # collected_songs = []
+    # not_genius = []
+    if collected_songs == [] and not_genius == []:
+        for song in songs:  # song = [Track, Artist]
+            song_find_attempts = 1
+            genius_song = None
 
-    for song in songs:  # song = [Track, Artist]
-        song_find_attempts = 0
-        genius_song = None
+            while not genius_song:
+                print("============ ATTEMPT " +
+                      str(song_find_attempts) + " ============")
 
-        while not genius_song and song_find_attempts < 5:
-            genius_song = api.search_song(song[0], song[1])
-
-            if genius_song is not None:
-                collected.append([song[0], song[1], genius_song.lyrics])
-            else:
-                genius_song = api.search_song(song[0], song[1], False)
-                if genius_song is not None:
-                    collected.append([song[0], song[1], genius_song.lyrics])
-
-            song_find_attempts += 1
-
-        # No results found for: '_________' <-- Acceptable
-        # Specified song does not have a valid URL with lyrics. Rejecting. <-- Error
-
-        if genius_song is None:
-            print("Song not found :(")
-            not_recognised.append([song[0], song[1]])
-
-    print("\n\n")
-
-    if len(not_recognised) == 0:
-        return collected
-    elif count > 3:
-        # Do something with not_recognised
-        return collected
+                genius_song = api.search_song(song[0], song[1])
+                if genius_song == "None_found":
+                    not_genius.append([song[0], song[1]])
+                elif genius_song is not None:
+                    collected_songs.append(
+                        [song[0], song[1], genius_song.lyrics])
+                else:
+                    song_find_attempts += 1
     else:
-        find_lyrics(not_recognised, collected, (count + 1))
+        print("Songs already found")
+    print("\n\n")
+    # return [collected_songs, not_genius]
+
+
+def lyric_search():
+    matching_songs.clear()
+    search_lyric = input("What is the lyric you are looking for?\n").lower()
+
+    collect_lyrics()
+
+    for song in collected_songs:
+        if search_lyric in song[2].lower():
+            matching_songs.append(song)
+
+    for song in matching_songs:
+        print("================================================")
+        print(song[0] + " by " + song[1])
+    print("================================================")
 
 
 if __name__ == "__main__":
     initial_import()
-    # find_lyrics(imported_song_data)
 
 # search_value = input("What word / phrase are you searching for? \n").lower()
--- spotify_data_scraper.py
+++ spotify_data_scraper.py
@@ -4,6 +4,7 @@ import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import shutil
+import sys
 
 Tk().withdraw()
 spotify_directory = "./spotify_data/"
@@ -23,14 +24,15 @@ def get_song_data():
     for file in os.listdir(spotify_directory):
         if file.endswith(".json"):
             raw_data = read(spotify_directory + file)
-
             for entry in raw_data:
-                entry_data = [entry["trackName"], entry["artistName"]]
-                if entry_data not in songs:
-                    songs.append(entry_data)
-                else:
-                    songs.remove(entry_data)
-                    songs.append(entry_data)
+                if entry["trackName"] != "Unknown Track" or entry["artistName"] != "Unknown Artist":
+                    entry_data = [entry["trackName"], entry["artistName"]]
+                    if entry_data not in songs:
+                        songs.append(entry_data)
+                    else:
+                        songs.remove(entry_data)
+                        songs.append(entry_data)
+            print("IMPORTED: " + spotify_directory + file)
 
     songs.reverse()
 
@@ -45,38 +47,44 @@ def get_spotify_file(fresh=False):
         title="Select Spotify JSON",
         filetypes=(("JSON", "*.json"), ("All Files", "*.*")))
 
-    if not fresh:
+    if file_location != '':
+        if not fresh:
+            resolved_input = False
+
+            while not resolved_input:
+                remove_old = input(
+                    "Do you want to remove pre-existing files? [y/N]\n").lower()
+                if remove_old == "y":
+                    for file in os.listdir(spotify_directory):
+                        os.remove(spotify_directory + file)
+                    print("Files removed.\n")
+                    resolved_input = True
+                elif remove_old == "n" or not remove_old:
+                    print("Files not removed.\n")
+                    resolved_input = True
+                else:
+                    print("Unknown Input\n")
+
         resolved_input = False
 
         while not resolved_input:
-            remove_old = input(
-                "Do you want to remove pre-existing files? [y/N]\n").lower()
-            if remove_old == "y":
-                for file in os.listdir(spotify_directory):
-                    os.remove(spotify_directory + file)
-                print("Files removed.\n")
+            copy_or_move = input(
+                "Do you want to copy or move the selected file? [C/m]\n").lower()
+            if copy_or_move == "c" or not copy_or_move:
+                shutil.copy(file_location, spotify_directory)
+                print("Files Copied.\n")
                 resolved_input = True
-            elif remove_old == "n" or not remove_old:
-                print("Files not removed.\n")
+            elif copy_or_move == "m":
+                shutil.move(file_location, spotify_directory)
+                print("Files Moved.\n")
                 resolved_input = True
+
             else:
                 print("Unknown Input\n")
 
-    resolved_input = False
-
-    while not resolved_input:
-        copy_or_move = input(
-            "Do you want to copy or move the selected file? [C/m]\n").lower()
-        if copy_or_move == "c" or not copy_or_move:
-            shutil.copy(file_location, spotify_directory)
-            print("Files Copied.\n")
-            resolved_input = True
-        elif copy_or_move == "m":
-            shutil.move(file_location, spotify_directory)
-            print("Files Moved.\n")
-            resolved_input = True
-        else:
-            print("Unknown Input\n")
+        return True
+    else:
+        return False
 
 
 def data_present():
